341332537d29860c105f02412bce61ce77ff72d5
Ivy Wong <ivy.wong022@gmail.com>
2015-01-23 12:26:00 -0500

    Get map to show up and edit comment styling

--- static/js/maptest.js
+++ static/js/maptest.js
@@ -1,35 +1,17 @@
 function generateMapProp(lat, lng) {
     var location = new google.maps.LatLng(lat,lng,true) 
     var mapProp = {
-	center: location,
-	zoom:16,
-	mapTypeId: google.maps.MapTypeId.ROADMAP
+        center: location,
+        zoom:16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    
-      var marker = new google.maps.Marker({
-	position: location,
-	map:map,
-	title: "test marker on the zip code"
+    var marker = new google.maps.Marker({
+        position: location,
+        map:map,
+        title: "test marker on the zip code"
     });
     marker.setMap(map);
-    
-}
-function initialize() {
-    //var address = '10282';
-    var address = {{zipcode}}
-    geocoder = new google.maps.Geocoder();
-    geocoder.geocode({'address':address}, function(results,status) {
-	if (status == google.maps.GeocoderStatus.OK) {
-	    var lat = results[0].geometry.location.lat();
-	    var lng = results[0].geometry.location.lng();
-	    generateMapProp(lat,lng);
-	} else {
-	    alert("Geocode was not successful for the following reason: " + status)
-	}
-    });
-
-    
 }
 
 function loadScript(){
@@ -39,4 +21,3 @@ function loadScript(){
     document.body.appendChild(script);
 }
 
-window.onload = loadScript;
