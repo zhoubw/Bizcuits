9a7a2f8ac2e93724e1965699b7ad74d0d280c01f
Ling Dong <lingd1944@gmail.com>
2015-01-26 16:21:27 -0500

    error handler 404

--- app.py
+++ app.py
@@ -253,6 +253,10 @@ def control():
 def error400(e):
     return render_template('400.html'), 400
 
+@app.errorhandler(404)
+def error404(e):
+    return render_template('404.html'), 404
+
 if __name__ == "__main__":
     app.debug = True
     #print client
